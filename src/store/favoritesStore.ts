6aca8e9f6f0a30625edc5b40c55b7e334dff3cac
Ruslan Skolotiy <dpruslan@gmail.com>
2023-05-24 11:24:16 +0300

    fix: some bugs

--- src/store/favoritesStore.ts
+++ src/store/favoritesStore.ts
@@ -3,10 +3,7 @@ import mockContacts from "../__data__/contacts.json";
 import { FavoriteContactsDto } from "src/types/dto/FavoriteContactsDto";
 
 export const favoritesStore = makeAutoObservable({
-    items: [
-        mockContacts[0].id,
-        mockContacts[1].id,
-        mockContacts[2].id,
-        mockContacts[3].id,
-    ] as FavoriteContactsDto,
+    items: mockContacts
+        .slice(0, 4)
+        .map((item) => item.id) as FavoriteContactsDto,
 });
